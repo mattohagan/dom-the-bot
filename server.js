038c282a4d7d0798432f5d1faae834a72c073c75
Matt O'Hagan <mattohagan@pal-nat186-92-27.itap.purdue.edu>
2015-10-17 10:07:10 -0500

    fix slack use

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var app = express();
 var dotenv = require('dotenv');
+var Slack = require('slack-client');
 
 app.set('port', (process.env.PORT || 5000));
 
 dotenv.load();
 
+var token = process.env.DOM_API_TOKEN;
+var autoReconnect = true;
+var autoMark = true;
+slack = new Slack(token, autoReconnect, autoMark)
+
 function authRequest(req, res, next){
 	var acceptable_tokens = [
-		process.env.SNAG-SLACK-TOKEN
+		process.env.SNAG_SLACK_TOKEN
 	];
 
 	// check if token is legit
@@ -24,10 +30,21 @@ app.all('*', authRequest);
 app.get('/slack/snag', function(req, res){
 	console.log(req.query);
 	console.log(req.body);
-
 });
 
 
 app.listen(app.get('port'), function() {
     console.log('Server running at port ' + app.get('port').toString());
 });
+
+
+
+slack.on('open', function(){
+	console.log('Connected to '+slack.team.name+' as '+slack.self.name);
+});
+
+slack.on('error', function(err){
+	console.log('Error'+err);
+});
+
+slack.login();
